a4555c59d8510fcbb70c98a3cd597baf7baa7f66
Dineshreddy28 <pelleti.d@northeastern.edu>
2024-04-12 20:57:31 -0400

    Adds admin and subs changes

--- admin_insert.sql
+++ admin_insert.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE PACKAGE application_user_pkg IS
-    -- Existing procedures...
     PROCEDURE insert_subs_plan(p_subscription_type IN subs_plan.subscription_type%TYPE, 
                                p_price IN subs_plan.price%TYPE, 
                                p_duration_time IN subs_plan.duration_time%TYPE, 
@@ -7,8 +6,15 @@ CREATE OR REPLACE PACKAGE application_user_pkg IS
                                
     PROCEDURE insert_location(p_location_name IN location.location_name%TYPE, 
                               p_loc_status IN location.loc_status%TYPE, 
-                              p_no_of_slots IN location.no_of_slots%TYPE, 
-                              p_address_address_id IN location.address_address_id%TYPE);
+                              p_no_of_slots IN location.no_of_slots%TYPE,
+                              p_street_address_1 IN location_address.street_address_1%TYPE,
+                              p_street_address_2 IN location_address.street_address_2%TYPE,
+                              p_city IN location_address.city%TYPE,
+                              p_state IN location_address.state%TYPE,
+                              p_country IN location_address.country%TYPE,
+                              p_zipcode IN location_address.zipcode%TYPE,
+                              p_latitude IN location_address.latitude%TYPE,
+                              p_longitude IN location_address.longitude%TYPE);
                               
     PROCEDURE insert_bike_status(p_status_name IN bike_status.status_name%TYPE);
     
@@ -18,7 +24,6 @@ END application_user_pkg;
 
 CREATE OR REPLACE PACKAGE BODY application_user_pkg IS
 
-    -- Procedure to insert data into subs_plan
     PROCEDURE insert_subs_plan(p_subscription_type IN subs_plan.subscription_type%TYPE, 
                                p_price IN subs_plan.price%TYPE, 
                                p_duration_time IN subs_plan.duration_time%TYPE, 
@@ -31,19 +36,32 @@ CREATE OR REPLACE PACKAGE BODY application_user_pkg IS
             dbms_output.put_line('Duplicate value for subscription plan.');
     END insert_subs_plan;
     
-    -- Procedure to insert data into location
     PROCEDURE insert_location(p_location_name IN location.location_name%TYPE, 
                               p_loc_status IN location.loc_status%TYPE, 
-                              p_no_of_slots IN location.no_of_slots%TYPE, 
-                              p_address_address_id IN location.address_address_id%TYPE) IS
+                              p_no_of_slots IN location.no_of_slots%TYPE,
+                              p_street_address_1 IN location_address.street_address_1%TYPE,
+                              p_street_address_2 IN location_address.street_address_2%TYPE,
+                              p_city IN location_address.city%TYPE,
+                              p_state IN location_address.state%TYPE,
+                              p_country IN location_address.country%TYPE,
+                              p_zipcode IN location_address.zipcode%TYPE,
+                              p_latitude IN location_address.latitude%TYPE,
+                              p_longitude IN location_address.longitude%TYPE) IS
+        v_location_id NUMBER;
     BEGIN
-        INSERT INTO location (location_name, loc_status, no_of_slots, address_address_id)
-        VALUES (p_location_name, p_loc_status, p_no_of_slots, p_address_address_id);
+        -- Insert into location and retrieve location_id
+        INSERT INTO location (location_name, loc_status, no_of_slots)
+        VALUES (p_location_name, p_loc_status, p_no_of_slots)
+        RETURNING location_id INTO v_location_id;
+
+        -- Insert into location_address using the new location_id
+        INSERT INTO location_address (location_location_id, street_address_1, street_address_2, city, state, country, zipcode, latitude, longitude)
+        VALUES (v_location_id, p_street_address_1, p_street_address_2, p_city, p_state, p_country, p_zipcode, p_latitude, p_longitude);
     EXCEPTION
         WHEN DUP_VAL_ON_INDEX THEN
-            dbms_output.put_line('Duplicate value for location.');
+            dbms_output.put_line('Duplicate value for location or location address.');
         WHEN OTHERS THEN
-            dbms_output.put_line('Error inserting location: ' || SQLERRM);
+            dbms_output.put_line('Error inserting location or location address: ' || SQLERRM);
     END insert_location;
     
     -- Procedure to insert data into bike_status
@@ -65,9 +83,11 @@ CREATE OR REPLACE PACKAGE BODY application_user_pkg IS
         WHEN DUP_VAL_ON_INDEX THEN
             dbms_output.put_line('Duplicate transaction type.');
     END insert_trans_type;
-
+    
+    -- Other procedures remain unchanged...
 END application_user_pkg;
 /
+
 
 
 BEGIN
@@ -80,11 +100,11 @@ BEGIN
     
     -- Assuming address_address_id values are placeholders and already exist.
     -- Insert data into location
-    application_user_pkg.insert_location('Beacon Hill', 'Active', 15, 1);
-    application_user_pkg.insert_location('Back Bay', 'Active', 20, 2);
-    application_user_pkg.insert_location('South Boston', 'Active', 10, 3);
-    application_user_pkg.insert_location('Seaport District', 'Active', 25, 4);
-    application_user_pkg.insert_location('Boston Common', 'Active', 30, 5);
+    application_user_pkg.insert_location('Roxbury', 'Active', 15, '115 Northampton','Roxbury','Boston','MA','USA','02118','42.361145','-71.057083');
+    application_user_pkg.insert_location('King Street', 'Active', 20, '14 John Eliot Square','Roxbury','Boston','MA','USA','02119','42.361145','-71.057083');
+    application_user_pkg.insert_location('Jamaica Plain', 'Active', 10, '44 Iffley','Jamaica Plain','Boston','MA','USA','02117','42.361145','-71.057083');
+    application_user_pkg.insert_location('Northeastern ', 'Active', 25, '213 Culinane','Huntington Ave','Boston','MA','USA','02115','42.361145','-71.057083');
+    application_user_pkg.insert_location('Boston Common', 'Active', 30, '200 Washington Street','Northeastern ','Boston','MA','USA','02118','42.361145','-71.057083');
     
     -- Insert data into bike_status
     application_user_pkg.insert_bike_status('Available');
